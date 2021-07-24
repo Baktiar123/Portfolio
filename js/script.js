0c47e68b4e6022a1b97aec265db1036325d642ec
Bakti <baktiarmahardika08@gmail.com>
2021-07-24 13:14:17 +0700

    Update bootstrap-grid.css, bootstrap-grid.css.map, and 47 more files...

--- js/script.js
+++ js/script.js
@@ -12,3 +12,51 @@ $('.page-scroll').on('click', function(e) {
  e.preventDefault();
 
 });
+
+
+$('.slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 350,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: false
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
