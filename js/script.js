624e11e5a4dd687844ede39363a7c17137861f2d
Baktiar Mahardika <baktiarmahardika08@gmail.com>
2021-08-25 15:01:43 +0700

    update index

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $('.page-scroll').on('click', function(e) {
  var elemenTujuan = $(tujuan);
 
  $('html , body').animate({
-  scrollTop: elemenTujuan.offset().top - 180
+  scrollTop: elemenTujuan.offset().top - 50
  },1250, 'easeInOutExpo');
 
  e.preventDefault();
